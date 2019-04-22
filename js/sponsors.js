9e559f35c7320dfaf0c75ddc50378a5443722e4b
Laura Wendlandt <wenlaura@umich.edu>
2019-04-22 10:34:45 -0400

    Change http to https

--- js/sponsors.js
+++ js/sponsors.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     //Dynamically load sponsors
      $.ajax({
         type: "GET",
-        url: "../data/sponsors.csv",
+        url: "/data/sponsors.csv",
         dataType: "text",
         success: function(data) {processSponsors(data,pageCategory);}
     });
